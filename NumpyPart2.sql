4ac57eac1d58634ae1786061c3ffc65aee278035
Mikael Rusin <mikaelgodlike@hotmail.com>
2018-09-01 20:40:39 +0200

    Command line, utility programs and UNIX integrations
    Mostly cmd line, Unix integrations and utility surrounding MySQL integrations

--- NumpyPart2.sql
+++ NumpyPart2.sql
@@ -438,8 +438,233 @@ SELECT * FROM isam_example ORDER BY groupings, id;
 # LOAD DATA INFILE '/local/access_log' INTO TABLE tbl_name
 # FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '\\'
 
- 
+#There are a number of programs related to MySQL overall:
+#
+#mysql - the mysql daemon, i.e, the mysql server.
+#
+#mysqld_safe - A server startup script. mysqld_safe attempts to start mysqld.
+#
+#mysql.server - A server startup script. Used on systems that use System V-style run directories containing
+#scripts that start system services for particular run levels. It invokes the mysqld_safe to start the MySQL Server.
+#
+#mysqld_multi - A server startup script that can start or stop multiple servers installed on the system.
 
+#comp_err - Used during the MySQL build/installation process. Compiles error messages files from the error source files.
 
-#https://dev.mysql.com/doc/refman/8.0/en/examples.html
+#mysql_secure_installation - Enables to improve the security of your MySQL installation.
 
+#mysql_ssl_rsa_setup - creates the SSL cert and key files and RSA key-pair files required to support secure connections, if those
+#files are missing. Files created by mysql_ssl_rsa_setup can be used for secure connections using SSL or RSA.
+
+#mysql_tzinfo_to_sql - This program loads the time zone tables in the mysql db using the contents of the host system zoneinfo db
+# (This is a set of files that describes time zones)
+
+#mysql_upgrade - Used after a MySQL upgrade operation. Checks tables for incompatibilies and repairs them if nessecary, and updates
+#the grant tables with any changes relevant in newer versionings of MySQL.
+
+#The following is the client programs that connect to the MySQL server:
+
+#mysql - cmd line tool for interactively entering SQL statements or executing them from a file in batch mode.
+
+#mysqladmin - A client that performs administrative operations, such as creating or dropping DBs, reloading the grant tables,
+#flushing tables to disk and reopening log files.
+#
+#mysqladmin can also be used to retrieve version, process and status information from the server. 
+
+#mysqlcheck - A table-maintenance client that checks, repairs and analyzes and optimizes tables.
+
+#mysqldump - A client that dumps a MySQL db into a file as SQL,text, or XML.
+
+#mysqlimport - A client that imports text files into their respective tables using LOAD DATA INFILE.
+
+#mysqlpump - a client that dumps a MySQL db into a file as SQL.
+
+#mysqlsh - MySQL Shell is a code editor for MySQL server. Allows for scripting of JS and Python.
+
+#mysqlshow - A client that displays information about DBs, tables, columns and indexes.
+
+#mysqlslap - A client made to emulate client load for a MySQL server and report the timing of each stage.
+#Works as if multiple clients are accessing the server.
+
+#There are a few MySQL Administrative and utility sectioned programs as well
+#
+#innochecksum - a offline InnoDB offline checksum utility.
+
+#myisam_ftdump - A utility that displays information about full-text indexes in MyISAM tables.
+
+#myisamchk - A utility to describe, check and optimize/repair MyISAM tables.
+
+#myisamlog - A utility that processes the contents of a MyISAM log file.
+
+#myisampack - A utiliy that compresses MyISAM tables to produce smaller read-only tables.
+
+#mysql_config_editor - A utility that enables you to store authentication credentials in a secure, encrypted login path file
+#named .mylogin.cnf
+
+#mysqlbinlog - A utility for reading statements from a binary log. The log of executed statements contained in the binary log files
+#can be used to help recover from a crash.
+
+#mysqldumpslow - A utility to read and summarize the contents of a slow query log.
+
+#Past this, there are MySQL program-development utilities.
+
+#mysql_config - A shell script that produces the option values needed when compiling MySQL programs.
+
+#my_print_defaults - A utility that shows which options are present in option groups of option files.
+
+#resolve_stack_dump - A utility program that resolves a numeric stack trace dump to symbols.
+
+#There are some misc. utilities:
+
+#lz4_decompress - A utility that decompresses mysqlpump output that was created using LZ4 compression.
+
+#perror - A utility that displays the meaning of system or MySQL error codes.
+
+#resolveip - A utility program that resolves a host name to an IP address or vice versa.
+
+#zlib_decompress - Utility that decompresses mysqlpump output that was created using ZLIB compression.
+
+#Some of the ENvironment variables used by MySQL are as follows:
+
+#MYSQL_UNIX_PORT - The default Unix socket file, used for connections to localhost
+
+#MYSQL_TCP_PORT - The default port number, used for TCP/IP connections
+
+#MYSQL_PWD - The default PW
+
+#MYSQL_DEBUG - Debug trace options when debugging
+
+#TMPDIR - The dir where temporary tables and files are created.
+
+#Past this, there is the interaction of actually interacting with MySQL from the Commandline
+
+#mysql --user=root test 
+#
+#Harkening back unto Unix systems, -- denotes commands, - denotes unix options
+#test is a DB name, in this instnace
+
+#most common operations shorthand to the first letter of the naming to denote command interaction
+#--host/-h - Host
+#--user/-u - User
+#--password/-p - Password
+#
+#--port/-P - Port number to connect to
+#--socket/-S - Specify a UNIX socket file on Unix (named Pipe name on Windows)
+
+#To circumvent full need of explicit path denotation in parameter accessing, we can include the installation Path
+#in the PATH setup.
+#
+#Meaning, if Mysql is installed to /usr/local/mysql/bin - we can put that into PATH, allowing us to simply denote it as 
+#mysql
+#
+#Generally the runtime path declaration resolves to a local handle in terms of the bin designation path
+#as in, just refering to bin with resolve it to a local designation path for MySQL.
+#
+#This can be important as well in terms of recursive path resolution, as this can come to be bugged.
+
+#The following pertains to connecting to a MySQL Server
+#
+# There are a number of default parameter handligns to fallback unto, in terms of how MySQL operates.
+#This means that if you just write for instance MySQL in the cmd, it will resolve to the default resolution
+#of certain program variables:
+
+#default host name: localhost
+#
+#default user name: ODBC on Windows, Unix login name on UNIX.
+#
+#if neither -p or --password is given, password is given as empty field
+#
+#default name of db to access: the first non option parameter found. If none, it does not select one
+
+#Example of showcasing of accessing
+#
+# mysql --host=localhost --user=myname --password=password mydb
+# mysql -h localhost -u myname -ppassword mydb
+
+#note, no space between -p or --password command and actual PW.
+
+#Other users on the system can see the pw entered on cmd line with ps auxw.
+
+#To avoid security risks in that department, simply omit writing the pw on the cmd line
+#
+# mysql --host=localhost --user=myname --password mydb
+# mysql -h localhost -u myname -p mydb
+
+# The above examples has omitted PW on the CMDline.
+
+#Some systems limit the PW to 8 chars. Either have the PW as 8 chars, or put it as a value in a option file
+
+#On Unix systems, localhost attempts to ordane connection through a UNix Socket. 
+#To circumvent this, use explicit IP declaration
+#
+# mysql --host=127.0.0.1
+# mysql --protocol=TCP Allows us to denote different connection types, even if it would default ot something else
+
+#If the server is made to accept Ipv6 connections, clients can connect with --host=::1
+
+#On windows, we can force named-pipe connections by specifying the --pipe or --protocol=PIPE option, or by using
+#. as the hostname
+
+#If named-pipe connections are not enabled, an error occurs. Use the --socket option to specify the
+#name of the pipe, if we do not wish to have the default naming on pipes.
+
+#Default port is 3306
+#mysql --host=remote.example.com Defaults to port being 3306
+
+#To specify a port use --port or -P
+#mysql --host=remote.example.com --port=13306
+
+#Do note, attempting to ordane localhost on a unix system will use a socket file
+#to circumvent this, use more explicit denotation
+
+#mysql --port=13306 --host=localhost Will ignore the port part on a Unix system, as it defaults to a socket file
+
+#mysql --port=13306 --host=127.0.0.1
+#mysql --port=13306 --protocol=TCP
+
+#We can utilize things akin to hints to denote what the client-side authentication plugin should be:
+#
+#--default-auth=plugin
+
+#To utilize pipes, we can use:
+#--pipe, -W
+#However, the server must be started with the --enable-named-pipe option to enable named-pipe connections.
+
+#We can denote what kind of protocol to use, which relates back unto what kind of a OS is permissible to utilize it
+#
+# --protocol=TCP - TCP/IP connection to local or remote server, usable by all
+#
+# --protocol=SOCKET - Unix socket file connection to local server, Unix only
+
+# --protocol=PIPE   - Named-pipe connection to local or remote server, Windows only
+
+# --protocol=MEMORY - Shared-memory connection to local server, Windows only
+
+#Do denote, for most cases - overhead is avoided with Socket default integrations in Unix systems.
+
+#To run with Shared memory:
+
+#--shared-memory-base-name=name , defaults to MYSQL. Server must be started with --shared-memory
+
+#--socket=file_name, -S file_name - On Unix, the Unix socket file to use, for connections made using a named pipe to a local server.
+# On Unix, defaults to /tmp/mysql.sock
+# 
+# On windows, the named pipe name to use. defaults to MySQL.
+
+#Server must be started with --enable-named-pipe to enable this.
+
+#--ssl* to start up connection based on SSL layering
+
+#--tls-version=protocol list
+#Denotes the protocols permitted by the client for encrypted connections. The value is a comma-separated list containing one
+#or more protocol names.
+
+#The protocols usable for this option depend on the SSL library used to compile MySQL.
+
+#--user=user name, -u user_name - username to be used. default on Windows is ODBC or the Unix login name on Unix.
+
+#We can modify options files akin to the Client section of an option file.
+
+#We can also utilize PATH variables, akin to MYSQL_HOST, USER or MYSQL_PWD.
+
+#https://dev.mysql.com/doc/refman/8.0/en/connecting-using-path.html
